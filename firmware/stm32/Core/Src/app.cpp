ee693d962cbd2ccd4a6beea31a2257f4e3f2b387
Robert Bicz <robertbicz@gmail.com>
2022-12-04 17:40:24 +0100

    Initial config, add stm32 project

---
+++ firmware/stm32/Core/Src/app.cpp
@@ -0,0 +1,21 @@
+/*
+ * app.cpp
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: Robert Bicz
+ */
+
+#include "main.h"
+
+
+
+extern "C" int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+
+	while (1)
+	{
+
+	}
+}
